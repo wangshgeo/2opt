88497085fdf15e0cf663803e218c99713cd8743e
Robert Lee <rlee32@gatech.edu>
2016-12-07 07:51:17 -0500

    KOpt under construction

--- simple/compile.sh
+++ simple/compile.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-g++ -std=c++11 TwoOpt.t.cpp TwoOpt.cpp IndexHash.cpp DistanceTable.cpp Reader.cpp Tour.cpp -o 2opt
-g++ -std=c++11 -fopenmp TwoOptParallel.t.cpp TwoOpt.cpp IndexHash.cpp DistanceTable.cpp Reader.cpp Tour.cpp -o 2optParallel
+#g++ -std=c++11 TwoOpt.t.cpp TwoOpt.cpp IndexHash.cpp DistanceTable.cpp Reader.cpp Tour.cpp -o 2opt
+#g++ -std=c++11 -fopenmp TwoOptParallel.t.cpp TwoOpt.cpp IndexHash.cpp DistanceTable.cpp Reader.cpp Tour.cpp -o 2optParallel
 
-g++ -std=c++11 -O3 -Wall ThreeOpt.t.cpp ThreeOpt.cpp IndexHash.cpp DistanceTable.cpp Reader.cpp Tour.cpp -o 3opt
+#g++ -std=c++11 -O3 -Wall ThreeOpt.t.cpp ThreeOpt.cpp IndexHash.cpp DistanceTable.cpp Reader.cpp Tour.cpp -o 3opt
 
-g++ -std=c++11 -O3 -Wall Sweeper.t.cpp Sweeper.cpp \
-    TwoOpt.cpp ThreeOpt.cpp \
+#g++ -std=c++11 -O3 -Wall Sweeper.t.cpp Sweeper.cpp \
+#    TwoOpt.cpp ThreeOpt.cpp \
+#    IndexHash.cpp DistanceTable.cpp Reader.cpp Tour.cpp -o sweep
+
+g++ -std=c++11 -O3 -Wall KOpt.t.cpp \
     IndexHash.cpp DistanceTable.cpp Reader.cpp Tour.cpp -o sweep
